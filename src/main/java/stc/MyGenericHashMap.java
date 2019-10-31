ea631c17c7afafee985aad8a474a5bee375684ec
mark-solo <mark.an.impostor@gmail.com>
2019-10-31 20:31:14 +0300

    implemented put function, created constructor, hash and add Entry util functions, and private Entry class

--- src/main/java/stc/MyGenericHashMap.java
+++ src/main/java/stc/MyGenericHashMap.java
@@ -5,11 +5,48 @@ import java.util.Map;
 import java.util.Set;
 
 public class MyGenericHashMap<K, V> implements Map<K, V> {
+    private int size;
+    private int capacity;
+    private float loadFactor;
+    private Entry<K, V>[] entries;
 
+    public MyGenericHashMap() {
+        this(16);
+    }
+
+    public MyGenericHashMap(int initialCapacity) {
+        this(initialCapacity, 0.75f);
+    }
+
+    public MyGenericHashMap(int initialCapacity, float loadFactor) {
+        this.size = 0;
+        this.capacity = initialCapacity;
+        this.loadFactor = loadFactor;
+        this.entries = new Entry[initialCapacity];
+    }
+
+    private int hash(K key) {
+        int h = key.hashCode() >>> 1;
+        return h & (capacity);
+    }
 
     // create/update
     @Override
     public V put(K key, V value) {
+        int hash = hash(key);
+
+        Entry<K, V> entry = entries[hash];
+
+        while (entry != null) {
+            if (entry.key.equals(key)) {
+                V old_value = entry.value;
+                entry.value = value;
+                return value;
+            }
+        }
+
+        entry = new Entry<K, V>(key, value);
+        addEntry(entry, entries, hash);
         return null;
     }
 
@@ -56,7 +93,7 @@ public class MyGenericHashMap<K, V> implements Map<K, V> {
     }
 
     @Override
-    public Set<Entry<K, V>> entrySet() {
+    public Set<Map.Entry<K, V>> entrySet() {
         return null;
     }
     // ----
@@ -74,5 +111,66 @@ public class MyGenericHashMap<K, V> implements Map<K, V> {
     // ------
 
     // utils
+    public void rehash() {
+
+    }
+
+    /* returns previous entry in bucket */
+    private Entry<K, V> addEntry(Entry<K, V> entry, Entry<K, V>[] bucketList, int hashIndex) {
+        if (entry == null || bucketList == null) return null;
+
+        if (bucketList[hashIndex] == null) {
+            bucketList[hashIndex] = entry;
+            return null;
+        }
+
+        Entry<K, V> curEntry = bucketList[hashIndex];
+        while (curEntry != null)
+            curEntry = curEntry.next;
+
+        curEntry.next = entry;
+        size++;
+        return curEntry;
+    }
+
+
     // -----
+
+    private class Entry<K, V> implements Map.Entry<K, V> {
+        private K key;
+        private V value;
+        private Entry next;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V old_value = this.value;
+            this.value = value;
+            return old_value;
+        }
+
+        public Entry<K, V> getNext() {
+            return next;
+        }
+
+        public Entry<K, V> setNext(Entry<K, V> next) {
+            Entry old_next = this.next;
+            this.next = next;
+            return old_next;
+        }
+    }
 }
